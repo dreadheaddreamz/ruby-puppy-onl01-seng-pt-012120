1bdcf07d6861e689d3158067dec8001a73279e57
James Batsell <jamesbatsell05@gmail.com>
2020-02-13 21:16:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog
+  @@all = []
+  def initialize
+    save
+  end
+  
+  def save
+    self << @@all
+  end
+  
+  def self.all
+    self
+  end
+  
+  
+  def self.print_all
+  @@all.each
+  puts @@all
+  end
+end
